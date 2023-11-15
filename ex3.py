224bcb4f25e3c38131b457d9ca4c62095e807f3d
florencia chao <fchao@itba.edu.ar>
2023-11-14 21:27:09 -0300

    fix ex 3

--- ex3.py
+++ ex3.py
@@ -15,9 +15,13 @@ def parseOrf(orfFile):
     hit_ids = []
 
     if hit_elements:
+        # add our nucleotido
+        ourHit = hit_elements[0].find("Hit_hsps/Hsp/Hsp_qseq").text
+        
         for hit_element in hit_elements[:10]:
             hit_id = hit_element.find("Hit_accession").text
             hit_ids.append(hit_id)
+        return ourHit, hit_ids
     else:
         print("No Hit elements found in the XML.")
         
@@ -36,13 +40,14 @@ if __name__ == "__main__":
     parser.add_argument("--fasta-msa", help="fasta prepared for msa", type=str, required=True)
     args = parser.parse_args()
 
-    accessions = parseOrf(args.input)
+    outHit, accessions = parseOrf(args.input)
     top10fastas = ''
     for accession in accessions:
         fasta = fetch_fasta_from_genbank(accession)
         top10fastas += ">%s\n%s\n" % (fasta.id, fasta.seq)
 
-    
+    top10fastas += ">%s\n%s\n" % (1, outHit)
+
 
     with open(args.fasta_msa, "w") as file:
       file.write(top10fastas)
